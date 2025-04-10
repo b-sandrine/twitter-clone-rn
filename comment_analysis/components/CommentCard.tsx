01de9ad6f2e3ca9974cb3008b98cdbd82b6f52a5
sandrine dushimimana <sanrdushimimana@gmail.com>
2025-04-10 13:11:58 +0200

    Verified text

--- comment_analysis/components/CommentCard.tsx
+++ comment_analysis/components/CommentCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 import Avatar from './Avatar';
 
@@ -7,10 +7,22 @@ import Avatar from './Avatar';
 export default function CommentCard({ comment, isAnalyticView }: { comment: any; isAnalyticView: boolean }) {
   return (
     <View style={styles.card}>
-      <Text style={styles.user}>
-        <Avatar uri={comment.user.avatar}></Avatar>
-        {comment.user.name} {comment.user.verified && '✔️'}
-      </Text>
+      <View style={styles.userContainer}>
+        <Avatar uri={comment.user.avatar} />
+        <View style={styles.userInfo}>
+          <Text style={styles.userName}>
+            {comment.user.name} {' '}
+            {comment.user.verified && (
+             <Image
+             source={require('../assets/images/verified.png')} // Path to the verified badge image
+             style={styles.verifiedBadge}
+           />
+          )}
+          {comment.sentiment === "positive" && '🟩' || comment.sentiment === "neutral" && '🟨' || comment.sentiment === "negative" && '🟥'}
+          </Text>
+        </View>
+      </View>
+      
       <Text>{comment.text}</Text>
       {isAnalyticView && <Text>Sentiment: {comment.sentiment}</Text>}
       {!isAnalyticView && <Text>Likes: {comment.likes}</Text>}
@@ -20,5 +32,22 @@ export default function CommentCard({ comment, isAnalyticView }: { comment: any;
 
 const styles = StyleSheet.create({
   card: { padding: 16, borderBottomWidth: 1, borderBottomColor: '#ddd' },
-  user: { fontWeight: 'bold' },
+  userContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+},
+userInfo: {
+    marginLeft: 10,
+},
+verifiedBadge: {
+  width: 20,
+  height: 20,
+  marginLeft: 4,
+  marginRight: 2
+},
+userName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+},
 });
